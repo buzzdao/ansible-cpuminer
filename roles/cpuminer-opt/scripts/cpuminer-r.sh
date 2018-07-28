f273dea686cd144e9b5a7726094fc5ebf441a87f
buzzdao <34927799+buzzdao@users.noreply.github.com>
2018-07-28 16:17:56 +0800

    hugepages=32

--- roles/cpuminer-opt/scripts/cpuminer-r.sh
+++ roles/cpuminer-opt/scripts/cpuminer-r.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 # /etc/init.d/cpuminer
 # Carry out specific functions when asked to by the system
+sudo sysctl -w vm.nr_hugepages=32
 sudo killall cpuminer
 cpumun=`cat /proc/cpuinfo| grep "processor"| wc -l`
 declare -i sum=$cpumun
